c958e703bb83a95191b3cdc8e581a88855d74f3e
Kamalika <59131714+kamalika0363@users.noreply.github.com>
2025-02-15 01:43:29 +0530

    dag_one.py

--- dags/dag_one.py
+++ dags/dag_one.py
@@ -4,8 +4,19 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
+
+default_args = {
+    'owner'                 : 'Kamalika',
+    'description'           : 'Use of the GitSync',
+    'depend_on_past'        : False,
+    'start_date'            : datetime(2025, 2, 15),
+    'email_on_failure'      : False,
+    'email_on_retry'        : False,
+    'retries'               : 1,
+    'retry_delay'           : timedelta(minutes=5)
+}
 # A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
+with DAG(dag_id="demo_dag", start_date=datetime(2025, 1, 1), schedule="0 0 * * *") as dag:
  # Tasks are represented as operators
  hello = BashOperator(task_id="hello", bash_command="echo hello")
 
