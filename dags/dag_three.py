492f916c28b37960664b9f8e0b4fed180d8bf808
Kamalika <59131714+kamalika0363@users.noreply.github.com>
2025-02-15 01:38:36 +0530

    dag_three.py

--- dags/dag_three.py
+++ dags/dag_three.py
@@ -1,1 +1,14 @@
+from datetime import datetime
 
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+
+with DAG(dag_id="dag_from_git_k", start_date=datetime(2025, 1, 1), schedule="0 0 * * *") as dag:
+ hello = BashOperator(task_id="hello", bash_command="echo hello")
+
+ @task()
+ def airflow():
+  print("airflow dag from gitsync to airflow localhost UI")
+
+ hello >> airflow()
