cc526b1aa33dd9486191d3e6dfc757a9fa662add
Kamalika <59131714+kamalika0363@users.noreply.github.com>
2025-02-17 15:57:51 +0530

    Create dag_5.py

---
+++ dags/dag_5.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+
+with DAG(dag_id="dag_branch_1", start_date=datetime(2025, 1, 1), schedule="0 0 * * *") as dag:
+    hello = BashOperator(task_id="hello", bash_command="echo hello")
+
+    @task()
+    def airflow():
+        print("branch dags, file dag_1")
+
+    hello >> airflow()
